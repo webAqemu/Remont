0f595f41367b7cb31e0cbaae9ef53d6cb74ba1d3
webAqemu <notych2812@gmail.com>
2020-07-16 23:04:41 +0300

    added new project to portfolio

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+
+	$('.comments__box').slick({
+		prevArrow: '<button class="comments__btn comments__btn--left"></button>',
+		nextArrow: '<button class="comments__btn comments__btn--right"></button>',
+		slidesToShow: 1,
+		responsive: [
+			{
+				breakpoint: 740,
+				settings: {
+					arrows: false,
+					dots: true,
+				}
+			},
+		]
+	});
+
+});
+
+jQuery(function ($) {
+	$("input[type='tel']").mask("+7 (999)999-99-99", { placeholder: "_" });
+});
+
+$('.call-btn').click(function () {
+	var popup_id = $('#' + $(this).attr("rel"));
+	$(popup_id).show();
+	$('.overlay-popup').show();
+});
+
+$('.overlay-popup').click(function () {
+	$('.overlay-popup, .popup').hide();
+});
